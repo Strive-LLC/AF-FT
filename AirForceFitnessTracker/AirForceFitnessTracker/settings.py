765d592a1254ed10c69192facbaf2b4afea1f11c
CKelsey95 <43053498+CKelsey95@users.noreply.github.com>
2018-09-10 18:16:03 -0600

    Update settings.py

--- AirForceFitnessTracker/AirForceFitnessTracker/settings.py
+++ AirForceFitnessTracker/AirForceFitnessTracker/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a70c20a9-b908-41a2-bb87-82e2f712b436'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
