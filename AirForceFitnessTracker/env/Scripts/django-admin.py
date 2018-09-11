6922c341d86a19bc9e8f7ef6a4ce597ba4293b19
Bilbo-T-Baggins1 <38269337+Bilbo-T-Baggins1@users.noreply.github.com>
2018-09-10 19:50:48 -0600

    test commit

---
+++ AirForceFitnessTracker/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\colton kelsey\Source\Repos\AF-FT2\AirForceFitnessTracker\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
