b9f659c2f5c382302ab61dac3c98a858c5b5560b
BigBear <38269264+IOIBigBearIOI@users.noreply.github.com>
2018-09-11 16:46:48 -0500

    Update for models
    Added units and made a....better set for the ages group

--- AirForceFitnessTracker/app/models.py
+++ AirForceFitnessTracker/app/models.py
@@ -5,11 +5,59 @@ Definition of models.
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=60)
     gender = models.CharField(max_length=1)
-    age = models.CharField(max_length=60)
-    paygrade = models.CharField(max_length=30)
+    age = models.CharField(max_length=2)
+    unit = models.CharField(max_length=60)
+    paygrade = models.CharField(max_length=5)
+
+    def __init__(self, name, gender, age, unit, paygrade):
+        return self.name + self.gender + self.age + self.unit + self.paygrade
     
+class Age_Groups(models.Model):
+    def Under_Thirty(self, age):
+        if self.age < 30:
+            return self.self
+        else:
+            pass            
+    Under_Thirty = models.CharField(max_length=10)
+
+    def Thirty_to_ThirtyNine(self, age):
+        if self.age >= 30:
+            if self.age < 40:
+                return self.self
+            else:
+                pass
+        else:
+            pass
+    Thirty_to_ThirtyNine = models.CharField(max_length=10)
+
+    def Forty_to_FortyNine(self, age):
+        if self.age >= 40:
+            if self.age < 50:
+                return self.self
+            else:
+                pass
+        else:
+            pass
+    Forty_to_FortyNine = models.CharField(max_length=10)
+
+    def Fifty_to_FiftyNine(self, age):
+        if self.age >= 50:
+            if self.age < 60:
+                return self.self
+            else:
+                pass
+        else:
+            pass
+    Fifty_to_FiftyNine = models.CharField(max_length=10)
+
+    def Sixty_and_Over(self, age):
+        if self.age > 60:
+            return self.self
+        else:
+            pass
+    Sixty_and_Over = models.CharField(max_length=10)
 
 class AFPT_Scores(models.Model):
     name = models.ForeignKey(User)
@@ -30,9 +78,5 @@ class AFPT_Scores(models.Model):
     BMI_points = models.CharField(max_length=4)
     BMI_HR = models.CharField(max_length=15)
 
-class Age_Groups(models.Model):
-    Under_Thirty = models.CharField(max_length=10)
-    Thirty_to_ThirtyNine = models.CharField(max_length=10)
-    Forty_to_FortyNine = models.CharField(max_length=10)
-    Fifty_to_FiftyNine = models.CharField(max_length=10)
-    Sixty_and_Over = models.CharField(max_length=10)
+    total_score = models.CharField(max_length=10)
+    total_HR = models.CharField(max_length=15)
